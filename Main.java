48ff4b6803984a28620aad98835f0601f7cd7375
daisylee101 <daisyl43@nycstudents.net>
2025-05-05 06:23:18 +0000

    donezooooo

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-helloooooo
+public class Main{
+    public static void main(String[] args) {
+        
+        Game one = new Game();
+        one.getLevel(1).reachGoal();
+        one.getLevel(1).setPoints(200);
+        one.getLevel(2).reachGoal();
+        one.getLevel(2).setPoints(100);
+        one.getLevel(3).reachGoal();
+        one.getLevel(3).setPoints(500);
+        one.makeBonus();
+
+        System.out.println(one.getScore());
+
+        Game five = new Game();
+        System.out.println(five.playManyTimes(4));
+    }
+}
